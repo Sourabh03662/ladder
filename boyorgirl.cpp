e7cdb43aee133edba2e024d2d5d0b1638d1ca458
Sourabh03662 <sourabhsaini03662@gmail.com>
2024-03-19 01:53:15 +0530

    boyorgirl.cpp

--- boyorgirl.cpp
+++ boyorgirl.cpp
@@ -4,13 +4,23 @@ using namespace std ;
 int main (){
     string s;
     cin>>s;
+    char arr[s.length()];
+    for (int i=0;i<s.length();i++){
+        arr[i]=s[i];
+    }
     int count=0;
     for (int i=0;i<s.length();i++){
-        if (s[i]==s[i+1]){
+        for (int j=i+1;j<s.length();j++){
+        if (arr[j]==arr[i]){
             count++;
         }
-        cout<<count<<endl;
     }
-    
+    }
+    int n=s.length()-count;
+    if (n%2==0){
+        cout<<"CHAT WITH HER!";
+    }else{
+        cout<<"IGNORE HIM!";
+    }
     return 0;
 }
